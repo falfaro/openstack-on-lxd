1398ef5b99242c317973d74fb6d56de5816d1715
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-03-30 13:08:13 +0200

    Align Nova flavors with those from conjure-up spell.

--- 00_nova_create_flavors.sh
+++ 00_nova_create_flavors.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source novarc
 openstack flavor create --public \
-    --ram   512 --disk  1 --ephemeral  0 --vcpus 1 m1.tiny
+    --ram   512 --disk   1 --ephemeral 0 --vcpus 1 m1.tiny
 openstack flavor create --public \
-    --ram  1024 --disk 20 --ephemeral 40 --vcpus 1 m1.small
+    --ram  2048 --disk  20 --ephemeral 0 --vcpus 1 m1.small
 openstack flavor create --public \
-    --ram  2048 --disk 40 --ephemeral 40 --vcpus 2 m1.medium
+    --ram  4096 --disk  40 --ephemeral 0 --vcpus 2 m1.medium
 openstack flavor create --public \
-    --ram  8192 --disk 40 --ephemeral 40 --vcpus 4 m1.large
+    --ram  8192 --disk  80 --ephemeral 0 --vcpus 4 m1.large
 openstack flavor create --public \
-    --ram 16384 --disk 80 --ephemeral 40 --vcpus 8 m1.xlarge
+    --ram 16384 --disk 160 --ephemeral 0 --vcpus 8 m1.xlarge
